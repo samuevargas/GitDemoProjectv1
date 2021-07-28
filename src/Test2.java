eceb4edf5650116406ff1d710c9650c343f8538f
Samuel.VargasC <samuel.vargasc@outlook.com>
2021-07-28 17:17:00 -0500

    Commit to the branch SamuelIntelliJ

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+
+public class Test2 {
+	    public static void Main(String[] args) {
+	        // write your code in Java SE 8
+	    	String S="Paola";
+	        int l=S.length();
+	        System.out.println(l);
+	        for (int i=1;i<=l-1;i++)
+	        {
+	        	for(int j=i+1;j<=l-i;j++)
+	            {
+	            if (S.charAt(i)==S.charAt(j))
+	            	{System.out.println(S.charAt(i));}
+	            }
+	        }
+	    }
+}
